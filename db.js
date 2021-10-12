bfaadaaa5aaaabf725aa6369dc75a9f03d37598d
mayowa1204 <oluwamayowaoseni12@gmail.com>
2021-10-12 17:48:54 +0100

    changes

--- db.js
+++ db.js
@@ -1,4 +1,9 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+    region:"eu-west-2",
+    accessKeyId:"accessKeyId",
+    secretAccessKeyId:"secretAccessKeyId",
+    endpoint:"http://localhost:8000"
+});
 
 module.exports = client;
